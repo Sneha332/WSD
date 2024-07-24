4bcc4fbdaaeb985c6309a90076745ca3745e5497
Sneha M <sneha.m@mca.christuniversity.in>
2024-07-24 21:40:29 +0530

    lab 2

---
+++ Lab2/file.py
@@ -0,0 +1,32 @@
+class bank:  
+    def __init__(self,account_no,balance):  
+        self._account_no = account_no
+        self._balance=balance
+  
+class get_balance(self):  
+    return self._balance
+def set_balance(self,balance):
+    if balance>0:
+        self._balance=balance
+    else:
+        raise valueerror("error") 
+
+class get_deposit(1000):
+    self._balance=balance+1000
+    return self._balance
+
+class get_deposit(1000):
+    self._balance=balance-1000
+     return self._balance
+  
+
+self._balance = 10000 
+print ("we will call the modified protected member outside the class: ",  
+            self._balance)  
+  
+  
+
+  
+obj_2 = bank()  
+  
+print ("Access the protected member of obj_2: ", obj_2._p)  
